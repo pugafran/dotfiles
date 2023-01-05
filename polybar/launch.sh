9d0cf6bd248475af2898f428d764eafd11605403
alexZeLoCO <alejandro.rod.lop@gmail.com>
2023-01-05 17:25:03 +0100

    Updated files

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,7 +8,8 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar HDMI 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar DP 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
