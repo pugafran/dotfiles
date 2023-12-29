95601cf2f0422be7e146d61a82e860afd997e4a5
Alex <alejandro.rod.lop@gmail.com>
2023-12-29 21:17:30 +0100

    Create setup-cli.sh

---
+++ setup-cli.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+echo "Cloning repository. This WILL take a while."
+git clone https://www.github.com/alexZeLoCO/dotfiles.git
+
+cp dotfiles/.vim ~/ -drv
+cp dotfiles/.config/fish ~/.config/ -drv
+cp dotfiles/
