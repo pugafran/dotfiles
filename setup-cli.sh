2b87c162bd3b643ca0ab6d25770c50d444e68564
Alex <alejandro.rod.lop@gmail.com>
2023-12-29 21:19:52 +0100

    Update setup-cli.sh

--- setup-cli.sh
+++ setup-cli.sh
@@ -3,5 +3,11 @@
 echo "Cloning repository. This WILL take a while."
 git clone https://www.github.com/alexZeLoCO/dotfiles.git
 
-cp dotfiles/.vim ~/ -drv
-cp dotfiles/.config/fish ~/.config/ -drv
+echo "Repository cloned. Copying configuration."
+
+cp dotfiles/.vim ~/ -dr
+cp dotfiles/.config/fish ~/.config/ -dr
+
+echo "Configuration copied. Use your package manager to install vim, npm and fish."
+echo "(sudo apt install vim npm fish / sudo pacman -S vim npm fish / sudo dnf install vim npm fish)"
+echo "Then use chsh to select /usr/bin/fish as your default shell"
